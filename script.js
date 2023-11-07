d58332690ab307554cecff5204d565268a879bc6
Richard Valente <rlvalente@gmail.com>
2023-11-07 18:08:16 -0300

    criado comando de repetiçao

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-var nome = "Ricard";
-var exp = 10000;
+const prompt = require('prompt-sync')();
 
+var nome = "";
+var exp = 0;
 var nivel;
+var num = 1;
 
-if (exp <= 1000) {
-  nivel = "Ferro";
-} else if (exp <= 2000) {
-  nivel = "Bronze";
-} else if (exp <= 5000) {
-  nivel = "Prata";
-} else if (exp <= 7000) {
-  nivel = "Ouro";
-} else if (exp <= 8000) {
-  nivel = "Platina";
-} else if (exp <= 9000) {
-  nivel = "Ascendente";
-} else if (exp <= 10000) {
-  nivel = "Imortal";
-} else {
-  nivel = "Raridade";
+function verificador(nome , exp){
+  if (exp <= 1000) {
+    nivel = "Ferro";
+  } else if (exp <= 2000) {
+    nivel = "Bronze";
+  } else if (exp <= 5000) {
+    nivel = "Prata";
+  } else if (exp <= 7000) {
+    nivel = "Ouro";
+  } else if (exp <= 8000) {
+    nivel = "Platina";
+  } else if (exp <= 9000) {
+    nivel = "Ascendente";
+  } else if (exp <= 10000) {
+    nivel = "Imortal";
+  } else {
+    nivel = "Raridade";
+  }
+
+  console.log(`o Heroi : ` + nome + ` está no nível ` + nivel);
+
 }
 
-console.log(`o Heroi de nome: ` + nome + `está no nível de ` + nivel);
+while(num != 0){
+  console.clear
+  console.log("Classificador de Nível");
+  
+  nome = prompt("Digite o nome do heroi: ");
+  exp = parseInt(prompt("Digite a experiencia do heroi: "));
+
+  verificador(nome, exp)
+
+  console.log("Deseja continuar?")
+  console.log("0 - SAIR / 1 - CONTINUAR")
+  num = parseInt("Continuar?")
+  
+}
